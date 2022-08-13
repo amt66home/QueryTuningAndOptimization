0b16417f4a3e6df5b04380e21bb066aa0abf099b
Anne Marie Taylor <89568179+amt66home@users.noreply.github.com>
2022-08-13 15:08:25 +0100

    Update StatisticsProfileOn.sql
    added a comment

--- StatisticsProfileOn.sql
+++ StatisticsProfileOn.sql
@@ -1,5 +1,6 @@
 /*
  Gives detailed info on the execution
+ Parse and compile time refers to the time it takes SQL Server to optimize the query
 */
 SET STATISTICS PROFILE ON
 SELECT * FROM Sales.SalesOrderDetail
